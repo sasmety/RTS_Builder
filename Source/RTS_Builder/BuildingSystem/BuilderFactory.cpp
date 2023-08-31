951cdd0182770f6f6799586630dfc0e84ada0521
sasmety <samet.k25455@gmail.com>
2023-08-31 23:56:45 +0300

    unnecessary switch remove

--- Source/RTS_Builder/BuildingSystem/BuilderFactory.cpp
+++ Source/RTS_Builder/BuildingSystem/BuilderFactory.cpp
@@ -14,19 +14,4 @@ UBuilderBase* UBuilderFactory::CreateBuilder(FBuildingData Data)
 	UBuilderBase* Builder = NewObject<UBuilderBase>(Data.BuilderClass);
 	Builder->Init(Cast<UGameManager>(UGameplayStatics::GetGameInstance(GetWorld())), Data);
 	return Builder;
-	// switch (Name)
-	// {
-	// case FName("Wall"):
-	// 	return NewObject<UBuilderWall>();
-	// case FName("Road"):
-	// 	return NewObject<UBuilderWall>();
-	// case FName("Static"):
-	// 	return NewObject<UBuilderWall>();
-	// case FName("Region"):
-	// 	return NewObject<UBuilderWall>();
-	// case FName("Attachment"):
-	// 	return NewObject<UBuilderWall>();
-	// default:
-	// 	return nullptr;
-	// }
 }
